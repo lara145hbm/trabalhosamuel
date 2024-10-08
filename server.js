15be2ee792cf56daeef330d67150793de6beebe9
unknown <lara_24032@ETEC.FURLAN>
2024-10-08 10:44:30 -0300

    primeirocommit

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const server = express()
+const bodyParser = require('body-parser')
+//Importar para ter acesso ao caminho dos arquivos
+const path = require('path')
+//Expor arquivos estáticos..
+server.use(express.static('trabalho'));
+server.use(express.json()) //Possibilidade de usar JSON
+
+server.use(bodyParser.urlencoded({ extended: true }))
+
+//EXemplo GET
+server.get('/add-novela', (req, res) => {
+    res.sendFile(path.join(__dirname, 'trabalho/add-novela.html'))
+})
+server.listen(3000, () => {
+    console.log("servidor no ar...")
+})
+server.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'trabalho/index.html'))
+})
+server.get('/list-novela', (req, res) => {
+    res.sendFile(path.join(__dirname, 'trabalho/list-novela.html'))
+})
